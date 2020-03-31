ce74a3747f32a53cbf1bca98ef057cd86c2ce3ba
Daniil_E <danny190100@gmail.com>
2020-03-31 12:03:22 +0300

    дни недели по номеру

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
         num = scanner.nextInt();
         } while ( num < 1 || num > 7);
         num-=1;
-        String day[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satuday", "Sunday"};
+        String day[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satuday", "Sunday"};//забыл про switch
         for (int i=0; i<7; i++){
             if (i==num) {
                 System.out.println(day[i]);

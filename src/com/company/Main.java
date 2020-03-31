65a43dd06adec46f0adae08e6a02e6d6b84cb5d9
Daniil_E <danny190100@gmail.com>
2020-03-31 11:58:22 +0300

    дни недели по номеру

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        do{
+        System.out.println("Enter number");
+        num = scanner.nextInt();
+        } while ( num < 1 || num > 7);
+        num-=1;
+        String day[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satuday", "Sunday"};
+        for (int i=0; i<7; i++){
+            if (i==num) {
+                System.out.println(day[i]);
+                break;
+            }
+        }
+    }
+}
